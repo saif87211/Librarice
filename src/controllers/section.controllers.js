c09202435d5f21a2571adc867f20412ce80d3881
Saif Ali <saifali87211@gmail.com>
2024-10-25 20:25:50 +0530

    add section controller

---
+++ src/controllers/section.controllers.js
@@ -0,0 +1,54 @@
+import { ApiResponse } from "../utils/ApiResponse";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Section } from "../models/section.model.js";
+
+const renderSection = asyncHandler(async (req, res) => {
+  const sections = await Section.find();
+
+  if (!sections) {
+    sections = [];
+  }
+  const apiResponse = new ApiResponse(200, { alert: false, sections });
+  return res.status(200).render("section", { apiResponse });
+});
+
+const addOrUpdateSection = asyncHandler(async (req, res) => {
+  const { sectionId, section } = req.body;
+  if (!section) {
+    const apiResponse = new ApiResponse(400, {
+      alert: true,
+      title: "Invlaid input",
+      message: "Please enter valid data",
+    });
+    return res.status(400).render("section", { apiResponse });
+  }
+
+  if (sectionId) {
+    await Section.findByIdAndUpdate(sectionId, { name: section });
+  } else {
+    await Section.Create({ name: section });
+  }
+
+  const apiResponse = new ApiResponse(200, {
+    alert: true,
+    title: sectionId
+      ? "Section updated successfully"
+      : "Section added successfully",
+    message: "",
+  });
+  return res.status(200).render("section", { apiResponse });
+});
+
+const deleteSection = asyncHandler(async (req, res) => {
+  const _id = req.body._id;
+
+  await Section.findByIdAndDelete(_id);
+
+  const apiResponse = new ApiResponse(200, {
+    alert: true,
+    title: "Entry was deleted Succesfully",
+  });
+  return res.status(200).render("section", { apiResponse });
+});
+
+export { renderSection, addOrUpdateSection, deleteSection };
